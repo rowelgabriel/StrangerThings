1de6560de0b11aaa57858606928d57f83fcf93a8
Rowel Gabriel <rowelgabrielvmangabat@gmail.com>
2025-12-17 20:44:04 +0800

    characters.js modified

--- characters.js
+++ characters.js
@@ -5,17 +5,19 @@ const getCharacters = async () => {
   const characterData = await response.json();
 
   for (var i = 0; i < characterData.length; i++) {
-
-    contents.innerHTML += 
-    `
-      <div class="col-6 col-md-4 col-lg-3">
-       <div class="card rounded-4 p-2 h-100 st-card">
-         <img src="${characterData[i].character.image.medium}">
+    const name = characterData[i].character.name;
+    const img = characterData[i].character.image.medium;
+    const actor = characterData[i].person.name;
+    contents.innerHTML +=
+      `
+    <div class="col-6 col-md-4 col-lg-3">
+      <div class="card rounded-4 p-2 h-100 st-card">
+        <img src="`+ img +`">
         <div class="card-body d-flex flex-column text-center">
-           <h5 class="card-title text-danger fw-semibold">${characterData[i].character.name}</h5>
-            <div class="card-text text-light">Played by ${characterData[i].person.name}</div>
-         </div>
-       </div>
+          <h5 class="card-title text-danger fw-semibold">`+ name +`</h5>
+          <div class="card-text text-light">Played by `+ actor +`</div>
+        </div>
+      </div>
     </div>
    `;
   }
